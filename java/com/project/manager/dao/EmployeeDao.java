930da077b20ba057c30228b0eb81685b52d12529
sagar <sagar.deshmukh3@cognizant.com>
2018-10-11 14:32:34 +0530

    Commit the DAO changes in the RESTfull service

--- java/com/project/manager/dao/EmployeeDao.java
+++ java/com/project/manager/dao/EmployeeDao.java
@@ -1,7 +1,15 @@
 package com.project.manager.dao;
 
+import java.util.List;
+
 import com.project.manager.model.User;
 
 public interface EmployeeDao {
    void save(User user);
+   
+   List<User> list();
+
+   void remove(User user);
+
+   void update(User user);
 }
